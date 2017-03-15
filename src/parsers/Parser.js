ed8f8ca310fb66dee8a16176526a3fdc4d2b8dbc
Leo Ding <leoding86@msn.com>
2017-03-15 22:42:19 +0800

    add 解析器基类

---
+++ src/parsers/Parser.js
@@ -0,0 +1,50 @@
+class Parser {
+
+    constructor (url, siteurl, sitelogo, resOrigin) {
+        this.url = url;
+        this.siteurl = siteurl;
+        this.sitelogo = sitelogo;
+        this.resOrigin = resOrigin;
+        this.totalPage = 0;
+        this.PROCESSING = 1;
+        this.COMPLETED = 2;
+
+        // 修改请求头信息
+        this.webRequestModifyHeader();
+    }
+
+    webRequestModifyHeader () {
+        let _this = this;
+        chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
+            details.requestHeaders.push({
+                name: "Referer",
+                value: _this.geturl()
+            });
+            let headers = details.requestHeaders;
+            return { requestHeaders: headers };
+        }, { urls: _this.resOrigin }, [ 'requestHeaders', 'blocking' ]);
+    }
+
+    geturl () {
+        return this.url;
+    }
+
+    getSiteurl () {
+        return this.siteurl;
+    }
+
+    getSitelogo () {
+        return this.sitelogo;
+    }
+
+    getTotalPage () {
+        return this.totalPage;
+    }
+
+    isValidIndex (index) {
+        return index <= this.totalPage - 1;
+    }
+
+}
+
+export default Parser;
