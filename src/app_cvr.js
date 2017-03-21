b010cfc04c17ed4ea9d344d124e42ed374c8bba1
Leo Ding <leoding86@msn.com>
2017-03-21 22:52:51 +0800

    update 创建event bus

--- src/app_cvr.js
+++ src/app_cvr.js
@@ -1,16 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+let _cvrBus = window._cvrBus = new Vue();
+
 /* Create a mount point */
 let $appContainer = document.createElement('div');
 $appContainer.id = 'cvr-app-container';
 $appContainer.style.position = 'fixed';
 $appContainer.style.top = '0';
 $appContainer.style.left = '0';
-$appContainer.innerText = '123';
 document.querySelector('body').appendChild($appContainer);
-
-let _cvrBus = window._cvrBus = new Vue();
 
 new Vue({
   el: '#cvr-app-container',
