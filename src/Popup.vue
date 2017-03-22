6ababf6d1bff9f31937e8376aa88255e2a225ef4
Leo Ding <leoding86@msn.com>
2017-03-22 15:08:36 +0800

    update 设置功能

--- src/Popup.vue
+++ src/Popup.vue
@@ -1,58 +1,38 @@
 <template>
     <div class="popup-content">
-        <div v-if="!showInfo">
-            <p class="not-supported">{{not_supported}}</p>
-        </div>
-        <div v-if="showInfo" class="site-info">
-            <div class="site"><img :src="logo" :alt="site"></div>
-            <div class="button" @click="startReadClickHandle">{{startRead}}</div>
-        </div>
+        <switcher :values="[0, 1]"
+                  :configTitle="_('debug_mode')"
+                  v-model="debugModeValue"></switcher>
     </div>
 </template>
 
 <script>
     import _ from './modules/_';
+    import Config from './modules/Config';
     import { UrlBuilder } from './modules/common';
 
     export default {
-        name: 'popup',
+        name : 'popup',
+
+        components : {
+            'switcher': require('./components/Switcher.vue')
+        },
 
         data () {
             return {
-                showInfo: false,
-                startRead: _('start_read'),
-                site: "unkown site",
-                logo: null,
-                parser: null,
-                requestUrl: null
-            }
+                debugModeValue: _cvrContainer.config.debug_mode
+            };
         },
 
-        computed: {
-            not_supported () {
-                return _('not_supported');
+        watch: {
+            debugModeValue (val) {
+                Config.set('debug_mode', val);
             }
         },
 
         beforeCreate () {
-            let _this = this;
-            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-                chrome.runtime.sendMessage({
-                    from: "popup",
-                    data: {
-                        op: "needData",
-                        tabId: tabs[0].id
-                    }
-                }, (response) => {
-                    console.log(response);
-                    if (response) {
-                        _this.showInfo = true;
-                        _this.site = response.data.site;
-                        _this.logo = response.data.logo;
-                        _this.requestUrl = response.data.url;
-                        _this.parser = response.data.parser;
-                    }
-                })
+            Config.get('debug_mode').then((val) => {
+                this.debugModeValue = val;
             });
         },
 
@@ -60,18 +40,9 @@
             
         },
 
-        methods: {
-            startReadClickHandle () {
-                if (this.requestUrl) {
-                    let url = new UrlBuilder(
-                        chrome.extension.getURL('./pages/comic_viewer.html')
-                    );
-                    url.setParams({
-                        url  : this.requestUrl,
-                        name : this.parser
-                    });
-                    chrome.tabs.create({ url: url.toString() });
-                }
+        methods : {
+            _ (str) {
+                return _(str);
             }
         }
     }
@@ -79,36 +50,10 @@
 
 <style lang="sass">
     body {
-        background: #101010;
+        background: #fff;
     }
 
     .popup-content {
-        width: 200px;
-
-        .not-supported {
-            color: #fff;
-            text-align: center;
-            font-size: 14px;
-        }
-
-        .site-info {
-            img {
-                width: 200px;
-            }
-
-            .button {
-                padding: 5px 0;
-                text-align: center;
-                background: #008fd4;
-                border: 1px solid #1ba7ec;
-                color: #f1faff;
-                font-size: 14px;
-                cursor: pointer;
-            }
-
-            .button:hover {
-                box-shadow: 0 0px 8px #9cdcfb
-            }
-        }
+        width: 120px;
     }
 </style>
