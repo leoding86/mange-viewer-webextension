1e75304c39fc5aa0e46a78cd1f581fe3d8e60982
Leo Ding <leoding86@msn.com>
2017-03-23 11:31:36 +0800

    update 添加初始化缩放等级设置

--- src/Popup.vue
+++ src/Popup.vue
@@ -1,8 +1,14 @@
 <template>
-    <div class="popup-content">
-        <switcher :values="[0, 1]"
-                  :configTitle="_('debug_mode')"
-                  v-model="debugModeValue"></switcher>
+    <div id="popup-container" class="row">
+        <div class="col-md-12">
+            <input-field class="input-field-row" :label="_('init_zoom_level') + '(1-3)'"
+                         :rule="['number','require']"
+                         v-model="initZoomValue"></input-field>
+            <switcher class="input-field-row"
+                      :values="[0, 1]"
+                      :configTitle="_('debug_mode')"
+                      v-model="debugModeValue"></switcher>
+        </div>
     </div>
 </template>
 
@@ -15,19 +21,28 @@
         name : 'popup',
 
         components : {
-            'switcher': require('./components/Switcher.vue')
+            'switcher': require('./components/Switcher.vue'),
+            'input-field': require('./components/InputField.vue') 
         },
 
         data () {
             return {
-                debugModeValue: _cvrContainer.config.debug_mode
+                debugModeValue: _cvrContainer.config['debug_mode'],
+                initZoomValue: _cvrContainer.config['init_zoom_level']
             };
         },
 
         watch: {
             debugModeValue (val) {
                 Config.set('debug_mode', val);
-            }
+            },
+
+            initZoomValue (val) {
+                if (!/^[1-9]\d*$/.test(val) || val > 3) {
+                    this.initZoomValue = val = 1;
+                }
+                Config.set('init_zoom_level', val);
+            },
         },
 
         beforeCreate () {
@@ -53,7 +68,12 @@
         background: #fff;
     }
 
-    .popup-content {
-        width: 120px;
+    #popup-container {
+        width: 150px;
+        padding: 0 6px;
+
+        .input-field-row {
+            margin: 10px auto;
+        }
     }
 </style>
