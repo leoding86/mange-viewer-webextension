01240f88f36fc38ecaece715e339ad3298c5f6ca
Leo Ding <leoding86@msn.com>
2017-03-23 11:32:50 +0800

    update 添加默认设置值

--- src/modules/Config.js
+++ src/modules/Config.js
@@ -1,17 +1,36 @@
+import Debug from '../components/CvrDebugEvent';
+
 let Config = {
     set (key, value) {
+Debug.emit('Set config \'' + key + '\' to ' + value);
         chrome.storage.sync.get('config', (items) => {
             if (!items.config) {
                 items.config = {};
             }
             items.config[key] = value;
-            chrome.storage.sync.set({'config': items.config});
+            chrome.storage.sync.set({'config': items.config}, () => {
+                if (chrome.runtime.lastError !== undefined) {
+Debug.emit('Config has been saved');
+                } else {
+Debug.emit('Save config failed. ' + chrome.runtime.lastError);
+                }
+            });
         });
     },
 
     get (key) {
         return new Promise((resolve, reject) => {
             chrome.storage.sync.get('config', (items) => {
+                if (!items.config) {
+                    items.config = this._config;
+                } else {
+                    for (let name in this._config) {
+                        if (!items.config[name]) {
+                            items.config[name] = this._config;
+                        }
+                    }
+                }
+
                 if (key === null) {
                     resolve(items.config ? items.config : null);
                 } else {
@@ -25,6 +44,11 @@ let Config = {
         chrome.storage.onChanged.addListener((changes, namespace) => {
             listener(changes, namespace);
         });
+    },
+
+    _config: {
+        'debug_mode'     : 0,
+        'init_zoom_level': 1
     }
 };
 
