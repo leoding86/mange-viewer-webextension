2fd10db5c644ca866db43f67200cfaf80b6ad60c
Leo Ding <leoding86@msn.com>
2017-03-26 23:08:22 +0800

    add 添加mangapark.me解析方法

---
+++ src/parsers/mangapark_me.js
@@ -0,0 +1,68 @@
+import * as Common from '../modules/common';
+import _ from '../modules/_';
+import BaseParser from './Parser';
+
+class Parser extends BaseParser {
+
+    constructor (url) {
+        super('mangapark_me', url);
+        return this.init();
+    }
+
+
+    init () {
+        this.url = this.pattern.exec(this.url)[1];
+        return new Promise((resolve, reject) => {
+            let magic = new Common.Magic();
+            this.totalPage = magic.getVar('_page_total', 'string');
+            resolve(this);
+        });
+    }
+
+    getImgSrc (page, callback, context) {
+        this.requestImgSrc(page).then((src) => {
+            callback.call(context, page, src);
+        });
+    }
+
+    getPageUrlByPage (page) {
+        return this.url + '/' + page;
+    }
+
+    requestImgSrc(page) {
+        let _this = this;
+        return new Promise((resolve, reject) => {
+            if (this.datasets[page] && this.datasets[page].status === this.COMPLETED) {
+                resolve(this.datasets[page].src);
+            } else if (!(this.datasets[page] && this.datasets[page].status === this.PROCESSING)) {
+                if (!this.datasets[page])
+                    this.datasets[page] = {};
+
+                this.datasets[page].status = this.PROCESSING;
+                let pageurl = _this.getPageUrlByPage(page);
+                let xhr = new XMLHttpRequest();
+                xhr.open('GET', pageurl);
+                xhr.onload = () => {
+                    let parser = new DOMParser();
+                    let doc = parser.parseFromString(xhr.responseText, 'text/html');
+                    let src = doc.querySelector('#img-1').src;
+
+                    this.datasets[page] = {
+                        status: _this.COMPLETED,
+                        src: src
+                    }
+                    resolve(src);
+
+                };
+                xhr.onerror = () => {
+                    reject(_('network_issue'));
+                }
+                this.datasets[page].xhr = xhr;
+                xhr.send(null);
+            }
+        });
+    }
+
+}
+
+export default Parser;
