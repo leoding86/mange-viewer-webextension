0af0ff558f53a50fe633fe363ab2ab4b93122c57
Leo Ding <leoding86@msn.com>
2017-03-28 20:49:03 +0800

    update 精简代码

--- src/content_script.js
+++ src/content_script.js
@@ -1,29 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import config from './modules/config';
-import { Matcher, TabStack } from './modules/common';
-
-window._cvrContainer = {};
+import { Matcher } from './modules/common';
 
 let matcher = new Matcher();
 matcher.setUrl(window.location.href);
+let parser = matcher.is();
 
-if (name = matcher.is()) {
-    /* Create a mount point */
-    let $appContainer = document.createElement('div');
-    $appContainer.id = 'cvr-app-container';
-    $appContainer.style.position = 'fixed';
-    $appContainer.style.top = '0';
-    $appContainer.style.left = '0';
-    document.querySelector('body').appendChild($appContainer);
+if (parser) {
+    window._cvrContainer = {};
+    window._cvrContainer.parser = parser;
 
-    /* load config from storage */
-    config.get(null).then((config) => {
-        window._cvrContainer.config = config;
-
-        new Vue({
-          el: '#cvr-app-container',
-          render: h => h(App)
-        })
-    });
+    require('./app');
 }
