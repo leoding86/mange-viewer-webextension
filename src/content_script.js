55e09548a5cd6349117f2f087ceb5651b21eebad
Leo Ding <leoding86@msn.com>
2017-03-28 08:56:21 +0800

    add 添加content script来判断页面

---
+++ src/content_script.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import App from './App.vue'
+import config from './modules/config';
+import { Matcher, TabStack } from './modules/common';
+
+window._cvrContainer = {};
+
+let matcher = new Matcher();
+matcher.setUrl(window.location.href);
+
+if (name = matcher.is()) {
+    /* Create a mount point */
+    let $appContainer = document.createElement('div');
+    $appContainer.id = 'cvr-app-container';
+    $appContainer.style.position = 'fixed';
+    $appContainer.style.top = '0';
+    $appContainer.style.left = '0';
+    document.querySelector('body').appendChild($appContainer);
+
+    /* load config from storage */
+    config.get(null).then((config) => {
+        window._cvrContainer.config = config;
+
+        new Vue({
+          el: '#cvr-app-container',
+          render: h => h(App)
+        })
+    });
+}
+
+    // if (message.action === 'can_run') {
+    //     /* Create a mount point */
+    //     let $appContainer = document.createElement('div');
+    //     $appContainer.id = 'cvr-app-container';
+    //     $appContainer.style.position = 'fixed';
+    //     $appContainer.style.top = '0';
+    //     $appContainer.style.left = '0';
+    //     document.querySelector('body').appendChild($appContainer);
+
+    //     /* load config from storage */
+    //     config.get(null).then((config) => {
+    //         window._cvrContainer.config = config;
+
+    //         new Vue({
+    //           el: '#cvr-app-container',
+    //           render: h => h(App)
+    //         })
+    //     });
+    // }
