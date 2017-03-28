e6d93998faa88e9b79295db6290708b7018afff4
Leo Ding <leoding86@msn.com>
2017-03-28 20:46:27 +0800

    update 添加more页面入口

--- webpack.config.js
+++ webpack.config.js
@@ -3,77 +3,70 @@ var webpack = require('webpack')
 var fs = require('fs')
 
 module.exports = {
-// entry: {
-//   main: './src/main.js',
-//   popup: './src/popup.js',
-//   background: './src/background.js',
-//   app_cvr: './src/app_cvr.js'
-// },
-entry () {
-    let entries = {
-        main: './src/main.js',
-        popup: './src/popup.js',
-        background: './src/background.js',
-        app_cvr: './src/app_cvr.js',
-        content_script: './src/content_script.js'
-    };
+    entry () {
+        let entries = {
+            popup: './src/popup.js',
+            background: './src/background.js',
+            content_script: './src/content_script.js',
+            more: './src/more.js'
+        };
 
-    return entries;
-},
-output: {
-    path: path.resolve(__dirname, './package/build'),
-    publicPath: '../build/',
-    filename: 'bundle.[name].js'
-},
-module: {
-    rules: [
+        return entries;
+    },
+    output: {
+        path: path.resolve(__dirname, './package/build'),
+        publicPath: '../build/',
+        filename: 'bundle.[name].js'
+    },
+    module: {
+        rules: [
+        {
+            test: /\.vue$/,
+            loader: 'vue-loader',
+            options: {
+                loaders: {
+                }
+        // other vue-loader options go here
+        }
+    },
     {
-        test: /\.vue$/,
-        loader: 'vue-loader',
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+    },
+    {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
         options: {
-            loaders: {
-            }
-// other vue-loader options go here
-}
-},
-{
-    test: /\.js$/,
-    loader: 'babel-loader',
-    exclude: /node_modules/
-},
-{
-    test: /\.(png|jpg|gif|svg)$/,
-    loader: 'file-loader',
-    options: {
-        name: '[name].[ext]?[hash]'
+            name: '[name].[ext]?[hash]'
+        }
+    },
+    {
+        test: /\.(woff2?|ttf|eot|svg)$/,
+        loader: 'url-loader',
+        options: {
+            limit: 10000
+        }
+    },
+    {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader']
     }
-},
-{
-    test: /\.(woff2?|ttf|eot|svg)$/,
-    loader: 'url-loader',
-    options: {
-        limit: 10000
-    }
-},
-{
-    test: /\.css$/,
-    loader: ['style-loader', 'css-loader']
-}
-]
-},
-resolve: {
-    alias: {
-        'vue$': 'vue/dist/vue.esm.js'
-    }
-},
-devServer: {
-    historyApiFallback: true,
-    noInfo: true
-},
-performance: {
-    hints: false
-},
-devtool: '#eval-source-map'
+    ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
@@ -106,5 +99,5 @@ if (process.env.NODE_ENV === 'production') {
         fs.unlinkSync(path.resolve(__dirname, 'package/build', file));
     });
 } else if (process.env.NODE_ENV === 'development') {
-    module.exports.devtool = '#eval';
+    module.exports.devtool = '#eval-source-map';
 }
