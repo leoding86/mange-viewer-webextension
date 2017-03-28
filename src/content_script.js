3bea6480fa15699e7054ad18da1037efae3b9ce4
Leo Ding <leoding86@msn.com>
2017-03-28 08:59:08 +0800

    update 删除无用代码

--- src/content_script.js
+++ src/content_script.js
@@ -27,23 +27,3 @@ if (name = matcher.is()) {
         })
     });
 }
-
-    // if (message.action === 'can_run') {
-    //     /* Create a mount point */
-    //     let $appContainer = document.createElement('div');
-    //     $appContainer.id = 'cvr-app-container';
-    //     $appContainer.style.position = 'fixed';
-    //     $appContainer.style.top = '0';
-    //     $appContainer.style.left = '0';
-    //     document.querySelector('body').appendChild($appContainer);
-
-    //     /* load config from storage */
-    //     config.get(null).then((config) => {
-    //         window._cvrContainer.config = config;
-
-    //         new Vue({
-    //           el: '#cvr-app-container',
-    //           render: h => h(App)
-    //         })
-    //     });
-    // }
