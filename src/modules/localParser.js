cc18c0a9d653fc7492231a9f09f2039b95dab1b1
Leo Ding <leoding86@msn.com>
2017-03-30 23:00:00 +0800

    update 添加rar,cbr文件支持

--- src/modules/localParser.js
+++ src/modules/localParser.js
@@ -19,40 +19,64 @@ class Parser {
             if (this.files.length === 0) {
                 reject(_('no_file_selected'));
             } else if (this.files.length === 1) {
-                JSZip.loadAsync(this.files[0]).then((zip) => {
+                if (/\.(zip|cbz)/.test(this.files[0].name)) {
+                    JSZip.loadAsync(this.files[0]).then((zip) => {
 
-                    /* 遍历文件 */
-                    let index = 0;
-                    let completedCount = 0;
-                    for (let filename in zip.files) {
-                        let file = zip.files[filename];
+                        /* 遍历文件 */
+                        let index = 0;
+                        let completedCount = 0;
+                        for (let filename in zip.files) {
+                            let file = zip.files[filename];
 
-                        /* 判断扩展名 */
-                        if (/\.(?:jpg|jpeg|png|gif|bmp|webp)$/i.test(file.name)) {
+                            /* 判断扩展名 */
+                            if (/\.(?:jpg|jpeg|png|gif|bmp|webp)$/i.test(file.name)) {
 
-                            /* hold position */
-                            this.datasets[index] = null;
+                                /* hold position */
+                                this.datasets[index] = null;
 
-                            file.async('blob').then((function (content) {
-                                _this.datasets[this.index] = {
-                                    name: name,
-                                    url: URL.createObjectURL(content)
-                                };
-                                completedCount++;
-                                _this.processCallback(completedCount);
+                                file.async('blob').then((function (content) {
+                                    _this.datasets[this.index] = {
+                                        name: name,
+                                        url: URL.createObjectURL(content)
+                                    };
+                                    completedCount++;
+                                    _this.processCallback(completedCount);
 
-                                if (completedCount == _this.datasets.length) {
-                                    resolve(_this);
-                                }
-                            }).bind({index: index, name: file.name}));
-                            index++;
+                                    if (completedCount == _this.datasets.length) {
+                                        resolve(_this);
+                                    }
+                                }).bind({index: index, name: file.name}));
+                                index++;
+                            }
+                        }
+
+                        this.totalPage = this.datasets.length;
+                    }, (e) => {
+                        reject(e);
+                    });
+                } else if (/\.(rar|cbr)/.test(this.files[0].name)) {
+                    let Worker = require("worker-loader!../workers/UnrarWorker");
+                    let worker = new Worker;
+                    let completedCounter = 0;
+                    worker.postMessage({file: this.files[0]});
+                    worker.onmessage = (event) => {
+                        if (event.data.count) {
+                            this.totalPage = event.data.count;
+                        } else {
+                            let blob = new Blob([event.data.content]);
+                            let url = URL.createObjectURL(blob);
+                            this.datasets.push({
+                                url: url
+                            });
+                            blob = null;
+                            this.processCallback(++completedCounter);
+
+                            if (this.totalPage <= completedCounter) {
+                                resolve(this);
+                            }
                         }
                     }
-
-                    this.totalPage = this.datasets.length;
-                }, (e) => {
-                    reject(e);
-                });
+                }
             } else {
                 reject(_('support_one_file_only'));
             }
